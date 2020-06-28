7baf65587cac748833e9dde2482cda9ac901d1ea
Kaito Yamada <38512851+KaitoYamadaAwake@users.noreply.github.com>
2020-06-28 23:54:52 +0900

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,10 +1,5 @@
-// クライアントからサーバーへの接続要求
 const socket = io.connect();
 
-// 接続時の処理
-// ・サーバーとクライアントの接続が確立すると、
-// 　サーバー側で、'connection'イベント
-// 　クライアント側で、'connect'イベントが発生する
 socket.on(
     'connect',
     () =>
@@ -12,7 +7,7 @@ socket.on(
         console.log( 'connect' );
     } );
 
-// 「Join」ボタンを押したときの処理
+// Join
 $( '#join-form' ).submit(
     () =>
     {
@@ -20,7 +15,6 @@ $( '#join-form' ).submit(
 
         if( $( '#input_nickname' ).val() )
         {
-            // サーバーに、イベント名'join' で入力テキストを送信
             socket.emit( 'join', $( '#input_nickname' ).val() );
 
             $( '#nickname' ).html( $( '#input_nickname' ).val() );
@@ -28,10 +22,10 @@ $( '#join-form' ).submit(
             $( '#chat-screen' ).show();
         }
 
-        return false;   // フォーム送信はしない
+        return false;   
     } );
 
-// 「Send」ボタンを押したときの処理
+// Send
 $( 'form' ).submit(
     () =>
     {
@@ -39,31 +33,23 @@ $( 'form' ).submit(
 
         if( $( '#input_message' ).val() )
         {
-            // サーバーに、イベント名'new message' で入力テキストを送信
             socket.emit( 'new message', $( '#input_message' ).val() );
 
-            $( '#input_message' ).val( '' );    // テキストボックスを空に。
+            $( '#input_message' ).val( '' );    
         }
-        return false;   // フォーム送信はしない
+        return false;  
     } );
 
-// サーバーからのメッセージ拡散に対する処理
-// ・サーバー側のメッセージ拡散時の「io.emit( 'spread message', strMessage );」に対する処理
+
 socket.on(
     'spread message',
-    //( strMessage ) =>
     ( objMessage ) =>
     {
-        //console.log( 'spread message :', strMessage );
+
         console.log( 'spread message :', objMessage );
 
-        // メッセージの整形
-        //const strText = objMessage.strDate + ' - ' + objMessage.strMessage;
         const strText = objMessage.strDate + ' - [' + objMessage.strNickname + '] ' + objMessage.strMessage;
 
-        // 拡散されたメッセージをメッセージリストに追加
-        //const li_element = $( '<li>' ).text( strMessage );
         const li_element = $( '<li>' ).text( strText );
-        $( '#message_list' ).prepend( li_element ); // リストの一番上に追加
-        //$( '#message_list' ).append( li_element );    // リストの一番下に追加
+        $( '#message_list' ).prepend( li_element ); 
     } );
