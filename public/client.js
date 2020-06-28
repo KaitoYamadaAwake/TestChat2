a2772dcf60f7ffabbca57058e8824dd3fd2fdae9
Kaito Yamada <38512851+KaitoYamadaAwake@users.noreply.github.com>
2020-06-29 00:00:58 +0900

    Update client.js

--- public/client.js
+++ public/client.js
@@ -12,16 +12,13 @@ $( '#join-form' ).submit(
     () =>
     {
         console.log( '#input_nickname :', $( '#input_nickname' ).val() );
-
         if( $( '#input_nickname' ).val() )
         {
             socket.emit( 'join', $( '#input_nickname' ).val() );
-
             $( '#nickname' ).html( $( '#input_nickname' ).val() );
             $( '#join-screen' ).hide();
             $( '#chat-screen' ).show();
         }
-
         return false;   
     } );
 
@@ -30,26 +27,20 @@ $( 'form' ).submit(
     () =>
     {
         console.log( '#input_message :', $( '#input_message' ).val() );
-
         if( $( '#input_message' ).val() )
         {
             socket.emit( 'new message', $( '#input_message' ).val() );
-
             $( '#input_message' ).val( '' );    
         }
         return false;  
     } );
 
-
 socket.on(
     'spread message',
     ( objMessage ) =>
     {
-
         console.log( 'spread message :', objMessage );
-
         const strText = objMessage.strDate + ' - [' + objMessage.strNickname + '] ' + objMessage.strMessage;
-
         const li_element = $( '<li>' ).text( strText );
         $( '#message_list' ).prepend( li_element ); 
     } );
